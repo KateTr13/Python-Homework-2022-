6fdfecf0394200f23e4ac7f6234f138ff3838f75
KateTr13 <teacup1313@gmail.com>
2022-09-22 10:49:53 +0300

    lesson3

--- KateBranchFile.py
+++ KateBranchFile.py
@@ -1,1 +1,20 @@
-1111
+text = 'hello\tworld'
+text = text.expandtabs(10)
+print(text)
+
+text = '----hello----'
+print(text.lstrip('-'))
+
+text = 'hello'
+print(text.removeprefix('he'))
+
+text = 'world'
+print(text.center(10, '_'))
+
+text = 'world'
+print(text.zfill(20))
+
+text = 'hello'
+print(text.ljust(10, '_'))
+
+
