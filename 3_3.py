15d56f67decbd81688fc68010d8ee37143f61b70
KateTr13 <teacup1313@gmail.com>
2022-09-23 17:51:31 +0300

    Homework lesson 3, N3 is ready

--- 3_3.py
+++ 3_3.py
@@ -2,8 +2,6 @@
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
 
-import collections
-
 text = int(input())
 see = {i: {'name': input(), 'email': input()} for i in range(0, text)}
 print(see)
