bff9103a73868549470eef1495a4a519a9408c1d
KateTr13 <teacup1313@gmail.com>
2022-09-26 16:55:17 +0300

    Homework lesson 4, N2 is ready(?)

--- 4_2.py
+++ 4_2.py
@@ -1,1 +1,20 @@
 # Сделать калькулятор: у пользователя спрашивается число, потом действие и второе число
+
+try:
+    Number1 = int(input('Введите первое число'))
+    Action = input('Введите действие (+, -, /, *)')
+    Number2 = int(input('Введите второе число'))
+    if Action == '+':
+        print(Number1+Number2)
+    elif Action == '-':
+        print(Number1-Number2)
+    elif Action == '/':
+        print(Number1/Number2)
+    elif Action == '*':
+        print(Number1*Number2)
+except ValueError:
+    print('Это не символ/Это не число')
+except Exception:
+    print('Попробуйте еще раз')
+
+
