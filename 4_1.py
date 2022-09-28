ba90da6a1fc1a91ed5a673be1c9cab362c6e30d6
KateTr13 <teacup1313@gmail.com>
2022-09-28 11:23:10 +0300

    Homework lesson 4, N2 is ready(?)

--- 4_1.py
+++ 4_1.py
@@ -1,1 +1,12 @@
-#Вывести первые N цисел кратные M и больше K
+#Вывести первые N чисел кратные M и больше K
+
+Number = int(input('Enter numberN'))
+Mult = int(input('Enter numberM'))
+K_lesser = int(input('Enter numberK'))
+while Number > K_lesser:
+      if Number % Mult:
+
+    print(i)
+
+
+
--- 4_3.py
+++ 4_3.py
@@ -1,2 +1,5 @@
 # Вывести четные числа от 2 до N по 5 в строку
 
+N_numbers = int(input())
+numbers = [i for i in range(2, N_numbers) if i % 2 == 0]
+numbers = str(numbers)
