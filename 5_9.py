8d34512971d584d0f550a7402b2cc8d53d81e51e
KateTr13 <teacup1313@gmail.com>
2022-10-02 15:04:55 +0300

    Lesson 5, homevork part2

--- 5_9.py
+++ 5_9.py
@@ -3,3 +3,15 @@
 #имена тех, у кого не указана почта (нет ключа email или значение этого ключа -
 #пустая строка)
 
+dictionary = {'dictionary1': {'name': 'Anton Bozensky', 'phone': '+375298760543', 'email': 'anton@gmail.com'},
+              'dictionary2': {'name': 'Innokentiy Sobolevsky', 'phone': '+375298760423', 'email': 'innokentiy@gmail.com'},
+              'dictionary3': {'name': 'Maria Lotz', 'phone': '+37523876542'}
+}
+
+for key, val in dictionary.items():
+    if not val.get("email"):
+        print(val.get("name"))
+
+
+
+
