4797065a39bb9f7c4a8b5ef4c4bcd359ab156511
KateTr13 <teacup1313@gmail.com>
2022-10-19 13:53:07 +0300

    Lection 10, sql homework 2

---
+++ CategoriesHomehowk2.sql
@@ -0,0 +1,50 @@
+
+Base = declarative_base()
+
+
+class Order(Base):
+    __tablename__: str = 'orders'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    status_id = (Integer, ForeignKey('statuses.id', ondelete='CASCADE')nullable=False)
+
+
+class Status(Base):
+    __tablename__: str = 'statuses'
+
+    id = Column(Integer, primary_key=True)
+    name = (VARCHAR(24), nullable=False, unique=True)
+
+
+class User(Base):
+    __tablename__: str = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = (VARCHAR(24), nullable=False)
+    email = (VARCHAR(24), nullable=False, unique=True)
+
+
+class Category(Base):
+    __tablename__: str = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = (VARCHAR(24), nullable=False, unique=True)
+
+
+class Product(Base):
+    __tablename__: str = 'products'
+
+    id = Column(Integer, primary_key=True)
+    title = (VARCHAR(36), nullable=False)
+    descr = (VARCHAR(140))
+    category_id = Column(Integer, ForeignKey('categories.id', ondelete='CASCADE'), nullable=False)
+
+
+class Order_item(Base):
+    __tablename__: str = 'order_items'
+
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('orders.id', ondelete='CASCADE'), nullable=False)
+    product_id = (Integer, ForeignKey('products.id', ondelete='CASCADE')nullable=False)
+
